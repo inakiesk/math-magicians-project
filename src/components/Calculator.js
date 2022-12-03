16324363a79e07c9e5892bd860dc712c5aacc53a
inakiesk <inakieskruiz@gmail.com>
2022-12-03 11:51:04 -0600

    Refactor Calculator component with hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,202 +1,197 @@
-import React from 'react';
+import { useState } from 'react';
 import calculate from './logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: '',
-      next: null,
-      operation: null,
-    };
-  }
+const Calculator = () => {
+  const [obj, setObj] = useState({
+    total: '',
+    next: null,
+    operation: null,
+  });
 
-  clickEvent = (e) => {
+  const clickEvent = (e) => {
     const data = e.target.value;
-    const state = calculate(this.state, data);
-    this.setState(state);
+    const state = calculate(obj, data);
+    setObj(state);
   };
 
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <div className="calculator">
-        <div className="displayScreen">
-          {total}
-          {operation}
-          {next}
+  const { total, next, operation } = obj;
+  return (
+    <div className="calculator">
+      <div className="displayScreen">
+        {total}
+        {operation}
+        {next}
+      </div>
+      <div className="calcBody">
+        <div className="calcRow">
+          <button
+            type="button"
+            value="AC"
+            onClick={clickEvent}
+            className="calcBtn"
+          >
+            AC
+          </button>
+          <button
+            type="button"
+            value="+/-"
+            onClick={clickEvent}
+            className="calcBtn"
+          >
+            +/-
+          </button>
+          <button
+            type="button"
+            value="%"
+            onClick={clickEvent}
+            className="calcBtn"
+          >
+            %
+          </button>
+          <button
+            type="button"
+            value="÷"
+            onClick={clickEvent}
+            className="calcBtn operator"
+          >
+            /
+          </button>
         </div>
-        <div className="calcBody">
-          <div className="calcRow">
-            <button
-              type="button"
-              value="AC"
-              onClick={this.clickEvent}
-              className="calcBtn"
-            >
-              AC
-            </button>
-            <button
-              type="button"
-              value="+/-"
-              onClick={this.clickEvent}
-              className="calcBtn"
-            >
-              +/-
-            </button>
-            <button
-              type="button"
-              value="%"
-              onClick={this.clickEvent}
-              className="calcBtn"
-            >
-              %
-            </button>
-            <button
-              type="button"
-              value="÷"
-              onClick={this.clickEvent}
-              className="calcBtn operator"
-            >
-              /
-            </button>
-          </div>
 
-          <div className="calcRow">
-            <button
-              type="button"
-              value={7}
-              onClick={this.clickEvent}
-              className="calcBtn"
-            >
-              7
-            </button>
-            <button
-              type="button"
-              value={8}
-              onClick={this.clickEvent}
-              className="calcBtn"
-            >
-              8
-            </button>
-            <button
-              type="button"
-              value={9}
-              onClick={this.clickEvent}
-              className="calcBtn"
-            >
-              9
-            </button>
-            <button
-              type="button"
-              value="x"
-              onClick={this.clickEvent}
-              className="calcBtn operator"
-            >
-              x
-            </button>
-          </div>
+        <div className="calcRow">
+          <button
+            type="button"
+            value={7}
+            onClick={clickEvent}
+            className="calcBtn"
+          >
+            7
+          </button>
+          <button
+            type="button"
+            value={8}
+            onClick={clickEvent}
+            className="calcBtn"
+          >
+            8
+          </button>
+          <button
+            type="button"
+            value={9}
+            onClick={clickEvent}
+            className="calcBtn"
+          >
+            9
+          </button>
+          <button
+            type="button"
+            value="x"
+            onClick={clickEvent}
+            className="calcBtn operator"
+          >
+            x
+          </button>
+        </div>
 
-          <div className="calcRow">
-            <button
-              type="button"
-              value={4}
-              onClick={this.clickEvent}
-              className="calcBtn"
-            >
-              4
-            </button>
-            <button
-              type="button"
-              value={5}
-              onClick={this.clickEvent}
-              className="calcBtn"
-            >
-              5
-            </button>
-            <button
-              type="button"
-              value={6}
-              onClick={this.clickEvent}
-              className="calcBtn"
-            >
-              6
-            </button>
-            <button
-              type="button"
-              value="-"
-              onClick={this.clickEvent}
-              className="calcBtn operator"
-            >
-              -
-            </button>
-          </div>
+        <div className="calcRow">
+          <button
+            type="button"
+            value={4}
+            onClick={clickEvent}
+            className="calcBtn"
+          >
+            4
+          </button>
+          <button
+            type="button"
+            value={5}
+            onClick={clickEvent}
+            className="calcBtn"
+          >
+            5
+          </button>
+          <button
+            type="button"
+            value={6}
+            onClick={clickEvent}
+            className="calcBtn"
+          >
+            6
+          </button>
+          <button
+            type="button"
+            value="-"
+            onClick={clickEvent}
+            className="calcBtn operator"
+          >
+            -
+          </button>
+        </div>
 
-          <div className="calcRow">
-            <button
-              type="button"
-              value={1}
-              onClick={this.clickEvent}
-              className="calcBtn"
-            >
-              1
-            </button>
-            <button
-              type="button"
-              value={2}
-              onClick={this.clickEvent}
-              className="calcBtn"
-            >
-              2
-            </button>
-            <button
-              type="button"
-              value={3}
-              onClick={this.clickEvent}
-              className="calcBtn"
-            >
-              3
-            </button>
-            <button
-              type="button"
-              value="+"
-              onClick={this.clickEvent}
-              className="calcBtn operator"
-            >
-              +
-            </button>
-          </div>
+        <div className="calcRow">
+          <button
+            type="button"
+            value={1}
+            onClick={clickEvent}
+            className="calcBtn"
+          >
+            1
+          </button>
+          <button
+            type="button"
+            value={2}
+            onClick={clickEvent}
+            className="calcBtn"
+          >
+            2
+          </button>
+          <button
+            type="button"
+            value={3}
+            onClick={clickEvent}
+            className="calcBtn"
+          >
+            3
+          </button>
+          <button
+            type="button"
+            value="+"
+            onClick={clickEvent}
+            className="calcBtn operator"
+          >
+            +
+          </button>
+        </div>
 
-          <div className="calcRow">
-            <button
-              type="button"
-              value={0}
-              onClick={this.clickEvent}
-              className="btn0"
-            >
-              0
-            </button>
-            <button
-              type="button"
-              value="."
-              onClick={this.clickEvent}
-              className="calcBtn"
-            >
-              .
-            </button>
-            <button
-              type="button"
-              value="="
-              onClick={this.clickEvent}
-              className="calcBtn operator"
-            >
-              =
-            </button>
-          </div>
+        <div className="calcRow">
+          <button
+            type="button"
+            value={0}
+            onClick={clickEvent}
+            className="btn0"
+          >
+            0
+          </button>
+          <button
+            type="button"
+            value="."
+            onClick={clickEvent}
+            className="calcBtn"
+          >
+            .
+          </button>
+          <button
+            type="button"
+            value="="
+            onClick={clickEvent}
+            className="calcBtn operator"
+          >
+            =
+          </button>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Calculator;
