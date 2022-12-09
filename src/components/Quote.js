f4f910fc140e7f64f6bf542b56b4aa8a405e9e30
inakiesk <inakieskruiz@gmail.com>
2022-12-09 11:53:33 -0600

    Add components for page links

---
+++ src/components/Quote.js
@@ -0,0 +1,13 @@
+import Navbar from './Navbar';
+
+const Quote = () => (
+  <div className="wrapper">
+    <Navbar />
+    <p className="quote">
+      &quot;Mathematics is not about numbers, equations, computations or algorithms;
+      it&apos;s about understanding.&quot; -William Paul Thurston
+    </p>
+  </div>
+);
+
+export default Quote;
